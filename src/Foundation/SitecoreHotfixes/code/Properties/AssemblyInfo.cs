95e0eac6ce06535b8b1fd60787d7ff1004455d31
Abhinav Singh <Abhinav.Singh@ghd.com>
2023-07-24 10:28:15 +0530

    update project name

--- src/Foundation/SitecoreHotfixes/code/Properties/AssemblyInfo.cs
+++ src/Foundation/SitecoreHotfixes/code/Properties/AssemblyInfo.cs
@@ -5,12 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GHD.Foundation.SitecoreHotfixes")]
+[assembly: AssemblyTitle("XYZ.Foundation.SitecoreHotfixes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("PublicisGroupe")]
-[assembly: AssemblyProduct("GHD.Foundation.SitecoreHotfixes")]
-[assembly: AssemblyCopyright("Copyright © PublicisGroupe 2023")]
+[assembly: AssemblyProduct("XYZ.Foundation.SitecoreHotfixes")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
